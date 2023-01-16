e01d3b0fb022d4dcb9bdf4466f59a92b397eaa1c
ch-sravan <110601810+ch-sravan@users.noreply.github.com>
2023-01-16 20:51:07 +0530

    Compute area of rectangle.c

---
+++ Compute area of rectangle.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    long long len,bre,area;
+    scanf("%lld%lld",&len,&bre);
+    area=len*bre;
+    printf("%lld",area);
+    return 0;
+}
